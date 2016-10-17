d2f48ad07af2daffb3f565a1a429df48d02c152e
thosd <thos.deaton@gmail.com>
2016-10-17 19:23:15 -0400

    added logging middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require ('express');
 var server = express ();
+var logger = require ('./middleware/logger.js');
+
 var port = process.env.PORT  || 8080;
 
 server.use(express.static(__dirname + '/public'));
+server.use(logger);
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', { root: __dirname });
