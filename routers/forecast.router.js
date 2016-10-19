71497ebb67edaddeb6385d8e740d4bef0e2de59f
thosd <thos.deaton@gmail.com>
2016-10-19 19:56:29 -0400

    damn son

--- routers/forecast.router.js
+++ routers/forecast.router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var apiKey = process.env.APIKEY || require('../config.js').APIKEY;
+var apiKey = process.env.APIKEY || require('../config.js').apiKey;
 var axios = require('axios');
 var authorize = require('../middleware/auth.js');
 var timeoutConfig = {
