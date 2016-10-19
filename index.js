ce9ff8d9190f9e25c407420ce88fbb7db7303e70
thosd <thos.deaton@gmail.com>
2016-10-19 19:13:28 -0400

    added routing functionality and updated for heroku

--- index.js
+++ index.js
@@ -4,30 +4,21 @@ var server = express();
 var logger = require('./middleware/logger.js');
 var axios = require('axios');
 var apiKey = require('./config.js').apiKey;
+//var authorize = require('./middleware/auth.js');
+
+var forecastRouter = require('./routers/forecast.router.js');
+var indexRouter = require('./routers/index.router.js');
 
 var port = process.env.PORT  || 8080;
 
-server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + './public'));
 server.use(logger);
 server.use(cors());
 
-server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', { root: __dirname });
-});
 
-server.get('/forecast/:latitude,:longitude', function(request, response){
-  var url = 'https://api.darksky.net/forecast/'+ apiKey +'/'+ request.params.latitude +',-122.43';
-  var timeoutConfig = {
-    timeout: 2000
-  };
-  axios.get(url, timeoutConfig)
-        .then(function(forecast){
-          response.send(forecast.data);
-        })
-        .catch(function(error){
-          response.send(error);
-        });
-});
+server.use(indexRouter);
+server.use(forecastRouter);
+
 
 server.listen(port, function(){
   console.log('Now listening on port...',port);
