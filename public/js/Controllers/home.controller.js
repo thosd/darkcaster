5d954bc426d0f7fd4b5c2257f2595ca3d262a1f8
thosd <thos.deaton@gmail.com>
2016-10-28 20:58:51 -0400

    added the ability to make http calls to the server using a service created the view for the current weather when a location is requested learned about filters to transform data in the dom used asset naming conventions to have response image selection

--- public/js/Controllers/home.controller.js
+++ public/js/Controllers/home.controller.js
@@ -2,15 +2,16 @@
   angular.module('forecast-app')
   .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$scope'];
+  HomeController.$inject = ['$scope', 'WeatherService'];
 
-  function HomeController($scope){
-    $scope.getWeatherURL = getWeatherURL;
+  function HomeController($scope, WeatherService){
+    $scope.getWeather = getWeather;
 
-    function getWeatherURL(newLatitude, newLongitude){
-      console.log(newLatitude);
-      console.log(newLongitude);
+    function getWeather(latitude, longitude){
+    WeatherService.getWeather(latitude, longitude)
+      .then(function(response){
+        $scope.weather = response.data;
+      });
     }
-
   }
 }());
